46eb845fbc2f9cc4b83cce24bbe8832bcb2a3b6c
benbenbenben404-school <73062112+benbenbenben404-school@users.noreply.github.com>
2021-10-31 17:28:02 +0000

    fgh

--- project/client/theme.js
+++ project/client/theme.js
@@ -1,6 +1,26 @@
 //THIS IS FOR TESTING PURPOSES ONLY
-document.onload = lighttheme()
+document.onload = setTheme()
 
-function lighttheme(){
+function setTheme(){
+    if (localStorage.getItem('theme') = "light"){
+        document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
     //document.documentElement.setAttribute('data-theme', 'light');
+}
+
+function toggleTheme(){
+    
+    if (localStorage.getItem('theme') == "light"){
+        localStorage.setItem('theme', 'dark');
+
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+
+        document.documentElement.setAttribute('data-theme', 'light');
+
+    }
 }
